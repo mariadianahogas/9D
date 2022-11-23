0a75272900f464b95e11f7ebbe5f47cd8999f792
mariadianahogas <hogasmaria33@yahoo.com>
2022-11-23 22:04:59 +0200

    numar prim

---
+++ Numar_prim/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int numarDivizori = 0;
+    for(int d = 1; d <= n; d++) {
+        if(n % d == 0) {
+            numarDivizori++;
+        }
+    }
+    if(numarDivizori == 2) {
+        cout << "DA";
+    } else {
+        cout << "NU";
+    }
+    return 0;
+}
